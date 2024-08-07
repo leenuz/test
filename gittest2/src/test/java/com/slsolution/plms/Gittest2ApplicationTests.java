7de33a06da8b5c3ef8a6964b3319b45f211162ad
jinsu lee <85428634+leenuz@users.noreply.github.com>
2024-08-07 11:20:32 +0900

    초기설정

---
+++ gittest2/src/test/java/com/slsolution/plms/Gittest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slsolution.plms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gittest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
